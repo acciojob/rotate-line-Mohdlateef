78f5e950d415149766678038af16132fe5eed7d8
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2023-12-30 00:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+
+const line=document.getElementById("line");
+let deg=2;
+setInterval(()=>{
+  deg=deg+2;
+  line.style.transform=`rotate(${deg}deg)`
+  
+  console.log("abc");
+},20)
